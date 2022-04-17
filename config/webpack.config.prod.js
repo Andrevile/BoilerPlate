e1fa9f7b4a2abdea4f3df64a84112f3c7152d9ce
abg3000 <abg3000@naver.com>
2022-04-17 16:06:10 +0900

    feat:copy-webpack-plugin, eslint, prettier

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,47 +1,47 @@
-const { merge } = require("webpack-merge");
-const path = require("path");
-const commonWebpack = require("./webpack.config.common");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserWebpackPlugin = require("terser-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const { merge } = require('webpack-merge');
+const path = require('path');
+const commonWebpack = require('./webpack.config.common');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(commonWebpack, {
-  mode: "production",
+  mode: 'production',
   output: {
-    path: path.resolve(__dirname, "../build"),
-    publicPath: "/",
-    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, '../build'),
+    publicPath: '/',
+    filename: '[name].[contenthash].js',
   },
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
 
   plugins: [
     new Dotenv({
-      path: ".env",
+      path: '.env',
     }),
     new CssMinimizerPlugin(),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: '[name].css',
     }),
     new CleanWebpackPlugin(),
   ],
   optimization: {
-    moduleIds: "deterministic",
+    moduleIds: 'deterministic',
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
+          name: 'vendors',
+          chunks: 'all',
         },
       },
     },
@@ -51,9 +51,9 @@ module.exports = merge(commonWebpack, {
           implementation: ImageMinimizerPlugin.imageminMinify, //imageminGenerate, Jpeg -> jpg 변환할 때 씀, 압축률은 그리 크지 않은듯
           options: {
             plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
             ],
           },
         },
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import "@/static/styles/reset.scss";
+import '@/static/styles/reset.scss';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 if (module.hot) {
-  module.hot.accept("./App", () => {
+  module.hot.accept('./App', () => {
     root.render(<App />);
   });
 }
